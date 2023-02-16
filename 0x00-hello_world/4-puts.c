d5ac967d7246d320e415b7616c3c126e7f83b49e
Isaac NSB. Kargbo <codewithnelson@gmail.com>
2023-02-16 07:58:49 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with pus function
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+retrun (0);
+}
