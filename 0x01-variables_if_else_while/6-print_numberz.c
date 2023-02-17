9463d3d21eb147364ae0f34aa992f7316259539e
Isaac NSB. Kargbo <codewithnelson@gmail.com>
2023-02-17 12:21:32 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
