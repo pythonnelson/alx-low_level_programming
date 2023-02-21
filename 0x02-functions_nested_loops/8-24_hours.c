945a14f8ccd96f3b6a100af979b954d628be1bc7
Isaac NSB. Kargbo <codewithnelson@gmail.com>
2023-02-21 10:36:48 -0800

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,7 +3,7 @@
  * jack_bauer - Prints every minute of the day of Jack Bauer
  * Return : ....
  */
-int jack_bauer(void)
+void jack_bauer(void)
 {
 	int a, b, c, d;
 
@@ -12,14 +12,14 @@ int jack_bauer(void)
 	for (b = a; b <= 9; b++)
 	{
 	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-	}
+	{
 	for (c = 0; c <= 5; c++)
 	{
 	for (d = 0; d <= 9; d++)
 	{
 	_putchar(a + '0');
 	_putchar(b + '0');
-	_putchar(50);
+	_putchar(58);
 	_putchar(c + '0');
 	_putchar(d + '0');
 	_putchar('\n');
