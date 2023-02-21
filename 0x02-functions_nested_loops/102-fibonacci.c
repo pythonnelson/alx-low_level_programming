109b63815ca78d887375e94d6f342484ab445b19
Isaac NSB. Kargbo <codewithnelson@gmail.com>
2023-02-21 11:49:03 -0800

    102-fibonnacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Prints the first 52 fibonnacci numbers
+ * Return: Nothing!
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 50)
+	{
+	if (i == 0)
+	printf("%d", j);
+	else if (i == 1)
+	printf(", %d", k);
+	else
+	{
+	k += j;
+	printf(", %ld", k);
+	}
+	++1;
+	}
+	printf("\n");
+	return (0);
+}
