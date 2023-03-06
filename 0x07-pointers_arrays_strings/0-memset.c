508863fcc3ad19110b4ca31db61c409589035d8a
Isaac NSB. Kargbo <codewithnelson@gmail.com>
2023-03-06 07:32:21 -0800

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _memset - fill a block of memory witha specific value
- * @s: starting address of memory to be filled 
+ * _memset - fill a block of memory with a specific value
+ * @s: starting address of memory to be filled
  * @b: the desired value
  * @n: number of bytes to be changed
  *
@@ -18,3 +18,4 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
+
