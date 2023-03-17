b813d53009d56d728c349cfd1d29c55053ab5b39
Isaac NSB. Kargbo <codewithnelson@gmail.com>
2023-03-17 15:45:19 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #define ERR_MSG "Error"
+
 /**
  * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
- *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
@@ -23,7 +23,6 @@ int is_digit(char *s)
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
  * Return: the length of the string
  */
 int _strlen(char *s)
@@ -47,16 +46,15 @@ void errors(void)
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
- * @argv: array of arguments
- *
+ * @agrv: array of arguments
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-	s1 = argv[1], s2 = argv[2];
 
+	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
@@ -72,7 +70,6 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
