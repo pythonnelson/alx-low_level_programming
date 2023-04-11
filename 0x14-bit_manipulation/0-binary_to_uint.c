2bf7ef2e213b0663d00a36bdffa23c280c82b968
Isaac NSB. Kargbo <codewithnelson@gmail.com>
2023-04-11 14:24:34 -0700

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,11 +2,10 @@
 
 /**
  * binary_to_unit - converts a binary number to unsigned int
- * @b: string containing the number
+ * @b: string containing the binary number
  *
  * Return: the converted number
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
@@ -14,14 +13,11 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
-
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-
 		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-
 	return (dec_val);
 }
